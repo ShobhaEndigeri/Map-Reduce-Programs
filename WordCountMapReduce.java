7c9394b3b1224a793fa3d8e24727ece1d2fdd221
Shobha <shobhaendigeri@gmail.com>
2017-09-14 15:40:49 +0530

    Update WordCountMapReduce.java

--- WordCountMapReduce.java
+++ WordCountMapReduce.java
@@ -14,7 +14,7 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 public class WordCountMapReduce {
 	
 	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-		private final static IntWritable one = new IntWritable();
+		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
 
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
